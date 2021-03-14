276b7f6d46356663cfea57da8ba23769fc7e5bef
Lendi Larici <lendi156@gmail.com>
2021-03-15 04:35:38 +0700

    First Document

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let tombol1 = document.querySelector('#link1');
+let page1 = document.querySelector('#bag1');
+
+tombol1.addEventListener('click', function() {
+  page1.scrollIntoView({behavior:"smooth"});
+});
+
+let tombol2 = document.querySelector('#link2');
+let page2 = document.querySelector('#bag2');
+
+tombol2.addEventListener('click', function() {
+  page2.scrollIntoView({behavior:"smooth"});
+});
+
+let tombol3 = document.querySelector('#link3');
+let page3 = document.querySelector('#bag3');
+
+tombol3.addEventListener('click', function() {
+  page3.scrollIntoView({behavior:"smooth"});
+});
+
+let tombol4 = document.querySelector('#link4');
+let page4 = document.querySelector('#bag4');
+
+tombol4.addEventListener('click', function() {
+  page4.scrollIntoView({behavior:"smooth"});
+});
